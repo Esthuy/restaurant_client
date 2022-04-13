27251204666f3ffdef49d4181bfadddab551ab73
Estelle <estellehuynen@yahoo.fr>
2022-04-13 16:22:06 +0200

    bug fix & css changes

--- src/app/component/display-one/display-one.component.ts
+++ src/app/component/display-one/display-one.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { REVIEW_INSERT_FORM } from 'src/app/form/review.form';
 import { Restaurant } from 'src/app/model/restaurant.model';
 import { Review } from 'src/app/model/review.model';
 import { RestaurantService } from 'src/app/services/restaurant.service';
+import { ReviewService } from 'src/app/services/review.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -15,14 +18,18 @@ export class DisplayOneComponent implements OnInit {
   restaurant! : Restaurant;  
   id : number; 
   connected! : boolean; 
-  reviews : Review[] = []; 
+  reviewToDisplay! : Review; 
+
+  reviewInsertForm : FormGroup; 
+  reviewToAdd! : Review; 
 
   constructor(
-     private service: RestaurantService, private userService: UserService,
-     route: ActivatedRoute, 
+     private service: RestaurantService, private userService: UserService, private reviewService : ReviewService,
+     route: ActivatedRoute, builder: FormBuilder,
      private router: Router) {
 
     userService.obsUser.subscribe(connected => this.connected = connected); 
+    this.reviewInsertForm = builder.group(REVIEW_INSERT_FORM); 
 
     const param_id = route.snapshot.paramMap.get('id');
     this.id = param_id ? parseInt(param_id) : -1;
@@ -35,15 +42,36 @@ export class DisplayOneComponent implements OnInit {
     
     }; 
    
-    //Comment faire dans le constructeur ? 
-    getReviews(){
-      this.reviews = this.restaurant.reviews; 
+
+    displayReview(review : Review){
+      this.reviewService.getOneReview(review.id).subscribe({
+        next : (review) => this.reviewToDisplay = review,
+        error: (err) => this.router.navigateByUrl('/restaurants')
+      })
     }
 
+    return(){
+      this.router.navigateByUrl('/restaurants');
+    }
+
+        //Ne pas oublier d'ajouter le restaurant et l'user
 
     addReview(){
-
+      if(this.reviewInsertForm.valid){
+        this.reviewToAdd = this.reviewInsertForm.value; 
+        this.reviewService.createReview(this.reviewToAdd).subscribe({
+          complete: () => {
+            this.reviewInsertForm.reset();  
+          },
+          error: err => alert("echec"),
+        }); 
     }
+  }
+
+    
+      
+      
+  
   
 
   ngOnInit(): void {
